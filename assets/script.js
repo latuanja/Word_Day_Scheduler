15d10511fe3e5a5c3924fffeaae99e089c1292b2
Music Box <latuanja.c@gmail.com>
2020-12-12 17:19:35 -0600

    Update to HTML with table, cells and button. Added footer for button, updated script links to correct ssources. Also updated CSS link, as it wasn't linking due to extra text.

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
 //code for current date variable
-var today = new Date();
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
 
-var n = weekday[document.getDay()];
-//code for date arrangment/formatting(M D Y)
-var date = d.getDay()+today.getMonth()+"-"+(today.getDate()+1)+"-"+today.getFullYear();
-document.getElementById("currentDay").innerHTML = date;
+
+//-----function 1
+
+//function needed to get value of div
+//need a class in the input tag to call out and push data to local storage
+//need a class/id for the hour that's pushed to local storage - when button is clicked, the info is pushed to local storage (function will be used)
+
+//------function 2
+//Start function that will update CSS/HTML by hour.  Will have a variable that holds the current hour (.hour method)
+//create variable for current time by using moment().get('hour');
+//create a control flow w/if/else statements to change colors
+//create var w/ times set in divs to compare w/current time
+//should be able to build local storage pull/push from this same function
