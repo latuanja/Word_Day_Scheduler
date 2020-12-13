87221cbcd02cd0690ddf601bfd28cc60bd9fd658
Music Box <latuanja.c@gmail.com>
2020-12-12 23:57:54 -0600

    Updated errors in JS code that didn't allow tables to appear and other errors that were a result of typos.

--- assets/script.js
+++ assets/script.js
@@ -10,8 +10,8 @@ $(document).ready(function () {
     //-----call variables and assign arrays for time(s)
     var plannedHours = [
         displayData = ["9AM", "10AM", "11AM", "12AM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM"],
-        hourNumbers = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-        //need a class/id for the hour that's pushed to local storage - when button is clicked, the info is pushed to local storage (function will be used)
+        hourData = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+       
     ];
     // how do i do text values???
     var savedTextArray = ["", "", "", "", "", "", "","","","","","","","","", ""];
@@ -32,12 +32,14 @@ $(document).ready(function () {
             timeDisplay.text(text[i]);
 
             //text areas variables. Create table data push to HTML
+            
             var textarea = $("<textarea class='textarea" + data[i] + "' rows='3' cols='60'>");
+            var textareaDisplay = $("<td class='description' >");
 
             textareaDisplay.append(textarea);
 
-            var saveBtnDisplay = $("<td class='saveBtn btn data-type=" + i + ">");
-            saveBtnDisplay.append($("i class='fa fa-floppy-o fa-3x'>"));
+            var saveBtnDisplay = $("<td class='saveBtn btn' data-type=" + i + ">");
+            saveBtnDisplay.append($("<i class='fa fa-floppy-o fa-3x'>"));
 
             //create rows
             newRow.append(timeDisplay, textareaDisplay, saveBtnDisplay);
@@ -46,35 +48,75 @@ $(document).ready(function () {
             checkTime(timeData, textarea);
             displayStoredText (textarea);
         }
-             console.log(savedTextArray)
+        console.log(savedTextArray);
 
     }
 
     function saveTextAreaOnClick() {
         $(".saveBtn").on("click", function () {
-            
+              //create var w/ times set in divs to compare w/current time
             var currentTimeData = $(this).parent().attr("data-type");
-
             var clickedTextarea = $(".textarea" + currentTimeData);
+            var rowToArrayCorrelation = $(this).attr("data-type");
 
-            var rowToArrayCorreclation = $(this).atttr("data-type");
-
+            savedTextArray[rowToArrayCorrelation] = clickedTextarea.val();
             storeTextInput();
+
         });
 
-        
     }
 
+    function clearAllOnClick () {
+        $("#clear-btn").on("click", function() {
+            localStorage.clear();
+            location.reload();
 
-    //need a class in the input tag to call out and push data to local storage
+        })
 
+    }
+        //need a class/id for the hour that's pushed to local storage - when button is clicked, the info is pushed to local storage (function will be used)
+       
+
+    function displayStoredText(textarea) {
+        var storedTextInput = JSON.parse(localStorage.getItem("savedtext"));
+
+        if (storedTextInput != null) {
+            savedTextArray = storedTextInput;
+            textarea.val(savedTextArray[i]);
+        };
+
+    };
+    
+    //create a control flow w/if/else statements to change colors
+
+    function checkTime(timeData, textarea) {
+
+        if (timeData === currentHour) {
+            textarea.addClass("present");
+
+        } 
+        else if (timeData < currentHour) {
+            textarea.addClass("past");
+        } 
+        else {
+            textarea.addClass("future");
+
+        };
+    }
+    //need a class in the input tag to call out and push data to local storage 
+    //should be able to build local storage pull/push from this same function
+    function storeTextInput() {
+        localStorage.setItem("savedtext", JSON.stringify(savedTextArray));
+    };
    
 
     //------function 2
     //Start function that will update CSS/HTML by hour.  Will have a variable that holds the current hour (.hour method)
+   displayTimeblocks(plannedHours[0], plannedHours[1]);
+   saveTextAreaOnClick();
+   clearAllOnClick ();
+    
+  
    
-    //create a control flow w/if/else statements to change colors
-    //create var w/ times set in divs to compare w/current time
-    //should be able to build local storage pull/push from this same function
 
 });
